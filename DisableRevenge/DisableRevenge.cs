584404d9639ec02b6b10177fd8896e7d72462bc4
Neoslyke <al_reminiscence@yahoo.com>
2026-03-19 15:27:56 +0800

    Moved .gitignore to the root of the repository

--- DisableRevenge/DisableRevenge.cs
+++ DisableRevenge/DisableRevenge.cs
@@ -11,7 +11,7 @@ namespace DisableRevenge {
 
         public override Version Version => new Version(1, 0, 1);
 
-        public override string Author => "Neoslyke (fork of Soofa";
+        public override string Author => "Neoslyke (fork of Soofa)";
 
         public override string Description => "Disables mobs picking up coins.";
 
